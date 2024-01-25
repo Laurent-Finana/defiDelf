ff0e92cbb68995d690cbaf4ffbf609e842929a31
Laurent-Finana <laurentfinanago@gmail.com>
2024-01-25 09:37:09 +0100

    Galerie photos en dur

--- src/Controller/Front/DisplayController.php
+++ src/Controller/Front/DisplayController.php
@@ -19,4 +19,14 @@ class DisplayController extends AbstractController
     {
         return $this->render('front/display/actions.html.twig');
     }
+
+    #[Route('/evenements', name: 'app_events')]
+    public function events(): Response
+    {
+        $photos = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+
+        return $this->render('front/display/events.html.twig', [
+            'photos' => $photos,
+        ]);
+    }
 }
