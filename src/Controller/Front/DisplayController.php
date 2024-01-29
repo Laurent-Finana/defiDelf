30111751f1c2d4e60ca1fba15ed6a98c77df2120
Laurent-Finana <laurentfinanago@gmail.com>
2024-01-29 11:41:45 +0100

    ajout de la homepage + modifications du contenu des onglets

--- src/Controller/Front/DisplayController.php
+++ src/Controller/Front/DisplayController.php
@@ -14,19 +14,25 @@ class DisplayController extends AbstractController
         return $this->render('front/display/homepage.html.twig');
     }
 
+    #[Route('/presentation', name: 'app_presentation')]
+    public function presentation(): Response
+    {
+        return $this->render('front/display/presentation.html.twig');
+    }
+
     #[Route('/actions', name: 'app_actions')]
     public function actions(): Response
     {
-        return $this->render('front/display/actions.html.twig');
+        $photos = ['1', '2', '4', '5', '6', '7', '8', '10'];
+
+        return $this->render('front/display/actions.html.twig', [
+            'photos' => $photos,
+        ]);
     }
 
     #[Route('/evenements', name: 'app_events')]
     public function events(): Response
     {
-        $photos = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-
-        return $this->render('front/display/events.html.twig', [
-            'photos' => $photos,
-        ]);
+        return $this->render('front/display/events.html.twig');
     }
 }
