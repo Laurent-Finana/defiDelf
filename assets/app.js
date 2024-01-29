5a1e87ee18470466997746300228f6404c240bc2
Laurent-Finana <laurentfinanago@gmail.com>
2024-01-29 17:23:21 +0100

    scss + couleurs

--- assets/app.js
+++ assets/app.js
@@ -5,10 +5,10 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/scss/app.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'bootstrap/bootstrap.index.js';
 
 // ...
 
