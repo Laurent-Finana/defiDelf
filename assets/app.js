8a6252fdebceb4d6d729e97a86f79c4715896a06
Laurent-Finana <laurentfinanago@gmail.com>
2024-01-31 12:27:27 +0100

    suppression sass, update page présentation

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,7 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/scss/app.css';
+import './styles/css/app.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/bootstrap.index.js';
