fa73aa22326eb02d83ff3c343cca0b2292dcb920
Laurent-Finana <laurentfinanago@gmail.com>
2024-02-27 15:54:00 +0100

    ajout des messages de contrainte en français

--- src/DTO/PracticeDTO.php
+++ src/DTO/PracticeDTO.php
@@ -6,39 +6,39 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class PracticeDTO
 {
-    #[Assert\NotBlank()]
+    #[Assert\NotBlank([],'Merci d\'entrer votre prénom')]
     #[Assert\Length(max: 200)]
     public string $firstname = '';
 
-    #[Assert\NotBlank()]
+    #[Assert\NotBlank([],'Merci d\'entrer votre nom')]
     #[Assert\Length(max: 200)]
     public string $lastname = '';
 
-    #[Assert\NotBlank()]
+    #[Assert\NotBlank([],'Merci d\'entrer votre email')]
     #[Assert\Email()]
     public string $email = '';
 
-    #[Assert\NotBlank()]
+    #[Assert\NotBlank([],'Merci d\'entrer votre numéro de téléphone')]
     public string $telephone_number_one = '';
 
     public string $telephone_number_two = '';
 
-    #[Assert\NotBlank()]
+    #[Assert\NotBlank([],'Merci d\'entrer votre nationalité')]
     #[Assert\Length(max: 100)]
     public string $nationality = '';
 
-    #[Assert\NotBlank()]
+    #[Assert\NotBlank([],'Merci d\'entrer votre adresse')]
     #[Assert\Length(max: 200)]
     public string $address = '';
 
     #[Assert\Length(max: 200)]
     public string $add_on_address = '';
 
-    #[Assert\NotBlank()]
+    #[Assert\NotBlank([],'Merci d\'entrer votre code postal')]
     #[Assert\Length(max: 50)]
     public string $postal_code = '';
 
-    #[Assert\NotBlank()]
+    #[Assert\NotBlank([],'Merci d\'entrer votre ville')]
     #[Assert\Length(max: 200)]
     public string $city = '';
 
