deea12250d2300b987da1ce0349f40695cdd499e
Laurent-Finana <laurentfinanago@gmail.com>
2024-03-12 15:19:53 +0100

    installation de CKEditor et mise en place pour Articles et Cours

--- src/Form/CourseType.php
+++ src/Form/CourseType.php
@@ -4,11 +4,10 @@ namespace App\Form;
 
 use App\Entity\Category;
 use App\Entity\Course;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -25,7 +24,7 @@ class CourseType extends AbstractType
             ->add('fileFile', FileType::class, [
                 'label' => 'Fichier à télécharger'
             ])
-            ->add('description', TextareaType::class, [
+            ->add('description', CKEditorType::class, [
                 'label' => 'Description',
                 'empty_data' => ''
             ])
